370c8d58f17d2ab4ed812cd330438dd87def7a71
Abdulrahmantommy <46884018+Abdulrahmantommy@users.noreply.github.com>
2020-12-17 13:02:56 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from snownlp import SnowNLP
+#for file Dataset
+Dataset = open(r"/home/tommy/Downloads/Reviews-Dataset.csv", mode='a', encoding='utf-8')
+
+ReviewsDataset = open(r"/home/tommy/Downloads/Reviews-Dataset.csv", 'r', encoding='utf-8')
+# here for read line in dataset file
+line = ReviewsDataset.readline()
+#num nlp
+sum=0
+count=0
+# run code
+
+while line:
+    print('\033[1m'+line, '\n')
+    s = SnowNLP(line)
+    print('{}'.format(s.sentiments))
+
+    sum+=s.sentiments
+    count+=1
+    line = ReviewsDataset.readline()
+
+score=sum/count
+#print dataset + finalscore
+print('finalscore={}'.format(score))
